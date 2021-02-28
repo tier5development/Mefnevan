467d5aec852f3c9847a9d865bb6442de9a13358a
Pirate0101 <prodipto.dhar@tier5.in>
2021-03-01 03:06:46 +0530

    Patch Release V1.0.3

--- front-end/public/profileGetter.js
+++ front-end/public/profileGetter.js
@@ -1,41 +1,113 @@
 
-//console.log("I am in Profile JS ");
-let UserFacebookid = "";
-let UserFacebookUsername  = "";
-let UserFacebookName  = "";
-let UserFacebookImage = "";
-let UserLoggedInFacebook  = false;
-//console.log("UserKyubiToken =====>",$('#mbasic_inline_feed_composer').length);
-// console.log("WindowId =====>",WindowId);
-// console.log("TabId =====>",TabId);
-if($('#mbasic_inline_feed_composer').length){
-    //console.log("User IS Logged In");
-    let UserURL= $(".ca").find("a").attr("href");
-    //console.log("User IS Logged In",UserURL);
-    UserURL = UserURL.substring(1, UserURL.length);
-    UserURL = UserURL.split('?');
-    let FacebookName  =  $(".ca a").find("img").attr("alt");
-    FacebookName  = FacebookName.split(',');
-    UserFacebookid  = $('input[name=target]').val();
-    UserFacebookUsername  = UserURL[0];
-    UserFacebookName  = FacebookName[0];
-    UserFacebookImage  =  $(".ca a").find("img").attr("src");
-    UserLoggedInFacebook  = true;
+console.log("I am in Profile JS ");
+let UserFacebookUsername =  "";
+let UserFacebookName    =   "";
+let UserFacebookid  =   "";
+let UserFacebookImage   =   "";
+let UserLoggedInFacebook =  false;
+let NavItem =$("nav").find("a");
+let CheckCounter =0;
+if(NavItem){
+    console.log("I Got THIS Nav Items ",NavItem.length);
+    if(NavItem.length  === 0){
+        CheckCounter =0;
+        UserLoggedInFacebook =  false;
+    }else{
+        NavItem.each( async function() {
+            let  name = $(this).text();
+            if(name === "Profile"){
+                CheckCounter =CheckCounter+1;
+            }
+        })
+        if(CheckCounter > 0){
+            UserLoggedInFacebook =  true;
+            let UserURL= $(".ca").find("a").attr("href");
+            if(UserURL){
+                let UserURLNo = UserURL.split('/');
+                let UserURLMo = UserURLNo[1].split('?');
+                UserFacebookUsername = UserURLMo[0];
+            }else{
+                let NewUserURL  =   $(".by").find("a").attr("href");
+                if(NewUserURL){
+                    let NewUserURLNo = NewUserURL.split('/');
+                    let NewUserURLMo = NewUserURLNo[1].split('?');
+                    UserFacebookUsername = NewUserURLMo[0];
+                }else{
+                    let NewUserURLNew  =   $(".bx").find("a").attr("href");
+                    if(NewUserURLNew){
+                        let NewUserURLNewNo = NewUserURLNew.split('/');
+                        let NewUserURLNewMo = NewUserURLNewNo[1].split('?');
+                        UserFacebookUsername = NewUserURLNewMo;
+                    }else{
+                        console.log("Warning I am Not Getting Facebook UserName Please Fix Me I AM bx !!!!");
+                    }
+                    console.log("Warning I am Not Getting Facebook UserName Please Fix Me I AM by !!!!");
+                }
+                console.log("Warning I am Not Getting Facebook UserName Please Fix Me I AM ca !!!!");
+            }
+            let Name  =  $(".ca a").find("img").attr("alt");
+            if(Name){
+                let finame = Name.split(',');
+                UserFacebookName  = finame[0];
+            }else{
+                let NewName  =  $(".by a").find("img").attr("alt");
+                if(NewName){
+                    let finameNew = NewName.split(',');
+                    UserFacebookName  = finameNew[0];
+                }else{
+                    let NewNameNew  =  $(".bx a").find("img").attr("alt");
+                    if(NewNameNew){
+                        let NewfinameNew = NewNameNew.split(',');
+                        UserFacebookName  = NewfinameNew[0];
+                    }else{
+                        console.log("Warning I am Not Getting Facebook Name Please Fix Me I Am bx!!!!");
+                    }
+                    console.log("Warning I am Not Getting Facebook Name Please Fix Me I Am by!!!!");
+                }
+                console.log("Warning I am Not Getting Facebook Name Please Fix Me I AM ca!!!!");
+            }
+            let fid =   $('input[name=target]').val();
+            if(fid){
+                UserFacebookid  = fid;
+
+            }else{
+                console.log("Warning I am Not Getting Facebook ID Please Fix Me !!!!");
+            }
+            let fimage =  $(".ca a").find("img").attr("src");
+            if(fimage){
+                UserFacebookImage  = fimage;
+            }else{
+                let Newfimage =  $(".by a").find("img").attr("src");
+                if(Newfimage){
+                    UserFacebookImage  = Newfimage;
+                }else{
+                    let NewfimageNew =  $(".bx a").find("img").attr("src");
+                    if(NewfimageNew){
+                        UserFacebookImage  = NewfimageNew;
+                    }else{
+                        console.log("Warning I am Not Getting Facebook ID Please Fix Me  I AM bx!!!!");
+                    }
+                    console.log("Warning I am Not Getting Facebook ID Please Fix Me  I AM by!!!!");
+                }
+                console.log("Warning I am Not Getting Facebook ID Please Fix Me I Am ca!!!!");
+            }
+          
+        }else{
+            UserLoggedInFacebook =  false;
+        }
+    }   
 }else{
-    //console.log("User IS Not Logged In");
-    UserFacebookid="";
-    UserFacebookUsername="";
-    UserFacebookName="";
-    UserFacebookImage="";
-    UserLoggedInFacebook=false;
+    UserLoggedInFacebook =  false;
+    console.log("I Did Not Got THIS Nav Items ",NavItem);
 }
+
 let parameters={
-    
     fb_id : UserFacebookid,
     fb_username : UserFacebookUsername,
     fb_name : UserFacebookName,
     fb_image  : UserFacebookImage,
-    fb_logged_id  : UserLoggedInFacebook,
-    
+    fb_logged_id  : UserLoggedInFacebook
   }
-  chrome.runtime.sendMessage({type: "storeUserInfoOrQueryThenStore", options: parameters});
+
+console.log("This I got After Scraping ",parameters);
+chrome.runtime.sendMessage({type: "storeUserInfoOrQueryThenStore", options: parameters});
